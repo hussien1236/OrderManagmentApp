6859038177f7c6fbf9e366a712b7307409542acd
unknown <hussienhamdan06@gmail.com>
2025-02-04 15:11:21 +0200

    Add load balancer to the app

--- Frontend/ordermanagement/src/Authentication/ApolloClient.ts
+++ Frontend/ordermanagement/src/Authentication/ApolloClient.ts
@@ -2,9 +2,18 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_API_SCHEMA_URL,
+  uri: getRandomGraphqlUrl(),
 });
-
+function getRandomGraphqlUrl() {
+  const urls = [
+    import.meta.env.VITE_GRAPHQL_URL_1,
+    import.meta.env.VITE_GRAPHQL_URL_2
+  ];
+  const randomIndex = Math.floor(Math.random() * 2);
+  console.log("Random index: "+randomIndex);
+  console.log("Random URL: "+urls[randomIndex]);
+  return urls[randomIndex];
+}
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token"); 
 
